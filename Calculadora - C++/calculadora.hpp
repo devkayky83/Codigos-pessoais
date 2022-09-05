168f19f81e2a4c255520b35099765a7b06903bdc
Kayky Júnio <104071389+devkayky83@users.noreply.github.com>
2022-09-05 19:03:04 -0300

    Create calculadora.hpp

---
+++ Calculadora - C++/calculadora.hpp
@@ -0,0 +1,16 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include "calculadora.cpp"
+
+double soma();
+double sub();
+double mult();
+double div();
+double rai();
+double porcentagem();
+double potencia();
+double trig();
+double menu_calculos();
+
+#endif
